96c5cc374f17698488e1e22102f1ae87f41b70a8
Nirmal S <nirmalpillai24@gmail.com>
2024-11-04 19:53:04 +0530

    json converter added

--- json_to_csv.py
+++ json_to_csv.py
@@ -13,10 +13,36 @@ base_path = pathlib.Path().absolute()
 input_file_path = base_path / sys.argv[1]
 output_file_path = base_path / "users.csv"
 
-file = open(output_file_path, "w")
-data = json.load(file)
-file.close()
+in_file = open(input_file_path, "r")
+out_file = open(output_file_path, "w")
+
+data = json.load(in_file)
+
+in_file.close()
+
+csv_writer = csv.writer(out_file)
+
+clean = lambda s: s.replace('\n', ' ').replace('\r', ' ') if s else ''
+
+csv_writer.writerow([
+        "login", "name", "company",
+        "location", "email", "hireable",
+        "bio", "public_repos", "followers",
+        "following", "created_at"])
 
 for item in data:
-    pass
+    csv_writer.writerow([
+        clean(item["login"]),
+        clean(item["name"]),
+        clean(item["company"]).lstrip('@').upper(),
+        clean(item["location"]),
+        clean(item["email"]),
+        clean(str(item["hireable"])),
+        clean(item["bio"]),
+        item["public_repos"],
+        item["followers"],
+        item["following"],
+        item["created_at"],
+    ])
 
+out_file.close()
